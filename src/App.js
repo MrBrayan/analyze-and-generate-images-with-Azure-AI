490155dfe466f7853c8e95f08f451e2c001c49f0
MrBrayan <destructorbn@gmail.com>
2023-11-27 14:06:30 -0500

    Diseñar la interfaz gráfica de usuario de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import React from 'react';
+/*import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  //const value = 'World';
+  // return <H1>Hello {value}</h1>;
+  return <div>
+      <h1>Computer Vision</h1>
+
+    </div>;
+}
+
+export default App;
+*/
+import React, { useState } from 'react';
+
+function App() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Lógica para el análisis de imágenes con la URL proporcionada
+    console.log('Análisis de imagen:', imageUrl);
+    // Aquí puedes añadir la lógica para el análisis de la imagen
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para la generación de imágenes con la URL proporcionada
+    console.log('Generación de imagen:', imageUrl);
+    // Aquí puedes añadir la lógica para la generación de imágenes
+  };
+
+  return (
+    <div>
+      <h1>Analizador y Generador de Imágenes</h1>
+      <p>Insert URL or type prompt:</p>
+      <input
+        type="text"
+        placeholder="Ingrese la URL de la imagen"
+        value={imageUrl}
+        onChange={handleImageUrlChange}
+      />
+      <br />
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+      <hr />
+    </div>
+  );
 }
 
 export default App;
